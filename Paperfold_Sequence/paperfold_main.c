cab87bfbe8ef8a561ffd10275c955d27e8c92117
Timothy Patton <timothyp.sci@gmail.com>
2018-05-07 14:36:27 -0400

    Complete argument input

--- Paperfold_Sequence/paperfold_main.c
+++ Paperfold_Sequence/paperfold_main.c
@@ -2,13 +2,22 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <math.h>
 
 char * paperfold(int i, char * str);
 
-int main(){
-    char str[50];
+int main(int argc, char * argv[]){
+    if(argc < 2){
+        printf("Please provide an argument in the form ./a.out #iter\n");
+        return -1;
+    }
+    int exp = 0;
+    sscanf(argv[1], "%d", &exp); 
+
+    char str[(int)(pow(2,exp))];
     str[0] = '\0';
-    paperfold(4, str);
+
+    paperfold(exp, str);
 }
 
 char * paperfold(int i, char * str) {
@@ -22,14 +31,15 @@ char * paperfold(int i, char * str) {
 
     //add inverse to end of str
     int end = strlen(str)-2;
-    while(end-- >= 0){
+    while(end >= 0){
         //TODO alternative to appendVal
         char appendVal[2];
         (str[end] == '0' ? (appendVal[0] = '1') : (appendVal[0] = '0'));
         appendVal[1] = '\0';
         strcat(str, appendVal);
+        end--;
     }
-    printf("%s\n", str);
+    printf("|--|%s\n", str);
 
     //recurse
     paperfold(i-1, str);
