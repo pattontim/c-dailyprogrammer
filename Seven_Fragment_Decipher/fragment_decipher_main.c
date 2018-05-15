f01fe08b2d00b1fdb5c5467dba8c6cd611f4f7e0
Timothy Patton <timothyp.sci@gmail.com>
2018-05-14 20:57:41 -0400

    Offset grid assignment to function

--- Seven_Fragment_Decipher/fragment_decipher_main.c
+++ Seven_Fragment_Decipher/fragment_decipher_main.c
@@ -2,11 +2,13 @@
 #include <math.h>
 
 char getDigitStateAtIndex(int gridNum, int digitIndex, char board[][28]);
+int getDigitAtGrid(int gridNum, char digitBoard[][28]);
 
 int main(){
     FILE *fp;
     char digitBoard[3][28];
 
+    //read in input board
     fp = fopen("./input.txt", "r");
     fgets(digitBoard[0], 255, (FILE*)fp);
     digitBoard[0][27] = '\0';
@@ -14,65 +16,50 @@ int main(){
     digitBoard[1][27] = '\0';
     fgets(digitBoard[2], 255, (FILE*)fp);
     digitBoard[2][27] = '\0';
-    
 
-    //getDigitStateAtIndex(1, 5, digitBoard);
-    //printf("\n");
-
-    /*    printf("%s\n", digitBoard[0]);
-          printf("%s\n", digitBoard[1]);
-          printf("%s\n", digitBoard[2]);*/
-
-    //printf("Test digit state: %c\n", getDigitStateAtIndex(4, 4, digitBoard)); 
-    int nums[10];
-
-    for(int i = 0; i<9; i++){
-        if(getDigitStateAtIndex(i, 1, digitBoard) == ' '){
-            if(getDigitStateAtIndex(i, 3, digitBoard) == ' '){
-                nums[i] = 1;
-            } else {
-                nums[i] = 4;
-            }
-        } else {
-            if(getDigitStateAtIndex(i, 3, digitBoard) == ' '){
-                //2, 3, 7
-                if(getDigitStateAtIndex(i, 4, digitBoard) == ' '){
-                    nums[i] = 7;
-                } else {
-                    if(getDigitStateAtIndex(i, 6, digitBoard) == ' ')
-                        nums[i] = 3;
-                    else
-                        nums[i] = 2;
-                }
-            } else {
-                if(getDigitStateAtIndex(i, 5, digitBoard) == ' '){
-                    //5, 6
-                    if(getDigitStateAtIndex(i, 6, digitBoard) == ' ')
-                        nums[i] = 5;
-                    else
-                        nums[i] = 6;
-                } else {
-                    //8, 9
-                    if(getDigitStateAtIndex(i, 6, digitBoard) == ' ')
-                        nums[i] = 9;
-                    else
-                        nums[i] = 8;
-                }
-            }
-        }
-    }
+    //print 
     for(int i = 0; i <9; i++){
-        printf("%d ", nums[i]);
+        printf("%d ", getDigitAtGrid(i, digitBoard));
     }
     printf("\n");
 }
 
+//grids are 3x3 squares, of which there are 9 in a board
+int getDigitAtGrid(int gridNum, char digitBoard[][28]){ 
+        if(getDigitStateAtIndex(gridNum, 1, digitBoard) == ' '){
+            if(getDigitStateAtIndex(gridNum, 3, digitBoard) == ' '){
+                return 1;
+            } else {
+                return 4;
+            }
+        } else {
+            if(getDigitStateAtIndex(gridNum, 3, digitBoard) == ' '){
+                if(getDigitStateAtIndex(gridNum, 4, digitBoard) == ' '){
+                    return 7;
+                } else {
+                    if(getDigitStateAtIndex(gridNum, 6, digitBoard) == ' ')
+                        return 3;
+                    else
+                        return 2;
+                }
+            } else {
+                if(getDigitStateAtIndex(gridNum, 5, digitBoard) == ' '){
+                    if(getDigitStateAtIndex(gridNum, 6, digitBoard) == ' ')
+                        return 5;
+                    else
+                        return 6;
+                } else {
+                    if(getDigitStateAtIndex(gridNum, 6, digitBoard) == ' ')
+                        return 9;
+                    else
+                        return 8;
+                }
+            }
+        }
+    }
 
 char getDigitStateAtIndex(int gridNum, int digitIndex, char board[][28]){
     int x = (gridNum*3) + (digitIndex%3);
-    //printf("The row number: %d\n", x); 
     int y = floor(digitIndex/3);
-    //printf("The col number: %d\n", y); 
-    //printf("%c", board[y][x]);
     return board[y][x];
 }
