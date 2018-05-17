ef35438fcacb3cf275d140efbbd671ec0ac90d00
Timothy Patton <timothyp.sci@gmail.com>
2018-05-16 22:41:13 -0400

    Add input parsing and sorting

--- Tally_Score/tally_main.c
+++ Tally_Score/tally_main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
+#include <stdlib.h>
 
 const int CHAR_START_U = 'A';
 const int CHAR_START_L = 'a';
@@ -10,15 +11,28 @@ typedef struct {
     int score;
 } player;
 
-int main(){
-    char * inputStr = "abcde";
+int cmpFunc(const void * a, const void * b){
+    int l = ((player *)a)->score;
+    int r = ((player *)b)->score;
+    return (r-l);
+}
 
+int main(int argc, char * argv[]){
+    if(argc < 2){
+        printf("Please provide a score to count.\n");
+        return -1;
+    }
 
+    char * inputStr = argv[1];
     player players[5]; 
+
+    //populate players with ids ranging from a to e
     for(int i = 0; i < 5; i++){
         players[i].id = 'a'+i;
         players[i].score = 0;
     }
+
+    //use the alphabet and offset to access array indices
     for(int i = 0; i < strlen(inputStr); i++){
         if(isupper(inputStr[i])){
             players[inputStr[i] - CHAR_START_U].score--;
@@ -26,10 +40,10 @@ int main(){
             players[inputStr[i]- CHAR_START_L].score++;
         }
     }
-
-    //qsort(players, 5, sizeof(player), cmpFunc); 
+    qsort(players, 5, sizeof(player), cmpFunc); 
 
     for(int i = 0; i < 5; i++){
-        printf("%c:%d\n", players[i].id, players[i].score);
+        printf("| %c:%d ", players[i].id, players[i].score);
     }
+    printf("\n");
 }
