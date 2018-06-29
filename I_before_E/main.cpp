d3fbbb5498e6ef38d9486d08e3e3caf32e7191eb
Timothy Patton <timothyp.sci@gmail.com>
2018-06-29 11:50:06 -0400

    Fix and simplify I before E logic

--- I_before_E/main.cpp
+++ I_before_E/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <fstream>
 #include <string>
+#include <algorithm>
 
 bool check(std::string word);
 
@@ -11,8 +13,34 @@ int main(){
  std::cout << check("sleigh");
  std::cout << check("nuclei");
  std::cout << check("reimburse");
+
+  std::string line;
+  std::ifstream myfile ("english.txt");
+  int exc = 0;
+  if (myfile.is_open()){
+    while (getline(myfile, line)){
+      if(!check(line))
+        exc++;
+    }
+    myfile.close();
+  }
+  else std::cout << "Unable to open file" << std::endl;
+
+
+  std::cout << std::endl << exc << std::endl;
+  return 0;
+
+ //check all english words
 }
 
 bool check(std::string word) {
-  return word.find("cei") != -1 || word.find("ei") == -1;
+  //TODO search all instances in words
+  std::transform(word.begin(), word.end(), word.begin(), ::tolower);
+
+  if(word.find("cie") != -1)
+    return false;
+  else if(word.find("cei") != -1)
+    return true;
+  else
+    return word.find("ei") == -1;
 }
